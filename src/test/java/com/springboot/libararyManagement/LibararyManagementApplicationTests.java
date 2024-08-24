9966ff296f857a08f6083f88e5e357de45d61852
sat0721 <63951201+sat0721@users.noreply.github.com>
2024-08-24 07:59:38 +0530

    project creation

---
+++ src/test/java/com/springboot/libararyManagement/LibararyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.libararyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibararyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
